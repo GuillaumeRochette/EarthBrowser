e7040d94a40e5334bddc85d62f84f00d78273823
Guillaume Rochette <rochette.guillaume@gmail.com>
2017-08-01 10:35:19 +0200

    o

--- src/ImageInfo.py
+++ src/ImageInfo.py
@@ -5,13 +5,14 @@ import matplotlib.pyplot as plt
 
 data_dir = "/home/guillaume/Documents/SegNet/data/Data_224x224/Data"
 i = 555
-s1 = gdal.Open("/home/guillaume/Documents/SegNet/data/Data_224x224/Data/Paris_RGB_PAN_00000_112_448.tif")
+s1 = gdal.Open("/home/guillaume/Documents/SegNet/data/RawData/Rio/Data/3band_AOI_1_RIO_img8.tif")
+s2 = gdal.Open("/home/guillaume/Documents/SegNet/data/RawData/Rio/Labels/3band_AOI_1_RIO_img8segcls.tif")
 print gdal.Info(s1)
 
 img_1 = np.array(s1.ReadAsArray())
-img_2 = img_1[..., ::-1, ::-1]
 img_1 = np.transpose(img_1, axes=[1, 2, 0])
-img_2 = np.transpose(img_2, axes=[1, 2, 0])
+
+img_2 = np.array(s2.ReadAsArray())
 
 print img_1.shape
 print img_1.dtype
